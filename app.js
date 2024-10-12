f8e1d785456313110dc684afc137fbe7a4fdc3e7
melawout <lobeloic98@gmail.com>
2024-10-12 12:01:27 -0600

    initial commit: first version of linktree clone

---
+++ app.js
@@ -0,0 +1,19 @@
+const shareButtons = document.querySelectorAll('.tile-share-button');
+
+
+async function copyText(e){
+  e.preventDefault()
+  const link = this.getAttribute('link')
+
+  try{
+    await navigator.clipboard.writeText(link)
+    alert("Copied the Text : " + link)
+  } catch(err){
+    console.error(err)
+  }
+}
+
+
+shareButtons.forEach(shareButton => 
+  shareButton.addEventListener('click', copyText)
+)
